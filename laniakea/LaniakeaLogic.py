eba2e4a33d3e37daf1ce9044aaa3eecc4e8d7f0c
Moritz Foglia <msfg76@ki5.mni.thm.de>
2025-07-08 19:27:01 +0200

    small fix for anti rotatable

--- laniakea/LaniakeaLogic.py
+++ laniakea/LaniakeaLogic.py
@@ -246,7 +246,7 @@ class Board():
         """Check whether the current player has any legal moves left.
         Returns True if there are legal moves, False otherwise.
         """
-        move_list = self.get_legal_moves(color)[0]
+        move_list = self.get_legal_moves(color)
         return any(move_list)
 
     
@@ -255,7 +255,7 @@ class Board():
         into the endzone of the opponent or if the opponent doesn't have any moves left
         @param color (1=white,-1=black)
         """
-        is_in_endzone = self.board[2 + (0 if color == 1 else 1)][6] == 5
+        is_in_endzone = self.board[2 + (0 if color == 1 else 1)][6] == 3
 
         opp_has_moves_left = self.has_legal_moves(-color)
 
