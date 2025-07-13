4e90ce1a003e88937b64e3058cdc5b8182edb3f6
Benedikt Fahn <vayegame@gmail.com>
2025-07-13 22:24:36 +0200

    Refactor AIPlayer initialization and action methods to improve model loading and action prediction

--- laniakea/pygame/ai/ai_setup.py
+++ laniakea/pygame/ai/ai_setup.py
@@ -1,5 +1,8 @@
 import numpy as np
 class AIPlayer:
+    """
+    AI player that uses a neural network to predict moves.
+    """
     def __init__(self, game, player, nnet, name, file=None):
         self.game = game
         self.nnet = nnet
@@ -11,16 +14,25 @@ class AIPlayer:
                 file = files[0]
             else:
                 raise FileNotFoundError("No model file found in the specified folder.")
+            
+        # Load the neural network model
         self.nnet.load_checkpoint(folder="models/" + name, filename=file)
         self.player = player
 
     def get_action(self, board):
+        """Get the next action for the AI player."""
+
+        # Convert board to canonical form
         tensor_board = self.nnet.board_to_tensor(board, self.player)
         can_board = self.game.getCanonicalForm(tensor_board, self.player)
         pi, v = self.nnet.predict(can_board)
         valids = self.game.getValidMoves(can_board, 1)
+
+        # Mask invalid moves
         pi = pi * valids
         sum_pi = np.sum(pi)
+
+        #Normalize probabilities
         if sum_pi > 0:
             pi /= sum_pi
 
@@ -29,10 +41,11 @@ class AIPlayer:
         self.last_action = move
         move = self.nnet.decode_action(move)
         if self.player == -1:
-            move = self.nnet.mirror_action(move)
+            move = self.nnet.mirror_action(move) # Mirror action for other player
         return move
 
     def get_last_action(self):
+        """Get the last action taken by the AI player."""
         move = self.nnet.decode_action(move)
         if self.player == -1:
             move = self.nnet.mirror_action(move)
